04bf08e5ce6369abc0a7ff15c16e75345e539394
Douglas McClean <douglas.mcclean@gmail.com>
2014-08-15 16:08:01 -0400

    Added bisection search function.

---
+++ atmos/Atmosphere/Math.hs
@@ -0,0 +1,33 @@
+module Atmosphere.Math
+( 
+  bisectionSearch
+)
+where
+
+-- We could import this functionality from one of several libraries, but it is very simple so we will save the dependency.
+-- Taken from https://www.fpcomplete.com/user/Sam567/computational-physics/beginner-s-tools/root-finding
+bisection epsilon f a b = let av = (b + a)/2 
+                               --the middle of the interval.
+                               fa = f a 
+                               --the function at a
+                               fav = f av 
+                               --the function at av
+                           in  if (b-a < epsilon) 
+                               --this means we are already close enough. 
+                               --In this case, we choose the less biased 
+                               --guess, which is av.
+                               then av
+                               else if (fa * fav < 0)
+                                    --this means the root is in the 
+                                    --interval (a,av)
+                                    then bisection epsilon f a av
+                                    else if (fav == 0) 
+                                    --this is almost impossible if your 
+                                    --initial intervals don't have any 
+                                    --relationship to the root, but in 
+                                    --practice, it happens.
+                                            then av
+                                            else bisection epsilon f av b 
+                                            --at this point, we know that
+                                            --the root is in the interval (av,b)
+                                            
